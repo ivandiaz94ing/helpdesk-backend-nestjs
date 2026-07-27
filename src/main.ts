e647a13a370ba86e832a4cfb3a39b9545b64f6dd
Ivan Arturo <ivandiaz94ing@gmail.com>
2026-07-27 10:29:38 -0500

    actualiza main.ts puerto

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
