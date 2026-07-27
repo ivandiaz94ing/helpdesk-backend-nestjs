ccb030720347a3003c705d92d2f759190a36532d
Ivan Arturo <ivandiaz94ing@gmail.com>
2026-07-26 22:34:09 -0500

    Agregando soporte SSL para la base de datos en producción

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,6 +18,7 @@ import { EquipoModule } from './equipo/equipo.module';
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
+        ssl: process.env.NODE_ENV === 'production' ? {rejectUnauthorized: false} : false,
         autoLoadEntities: true,
         synchronize: true,
         // entities: [User]
