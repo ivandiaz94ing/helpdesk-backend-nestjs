fdda57438746d954e44e2b77a6cebd6b653570da
Ivan Arturo <ivandiaz94ing@gmail.com>
2026-07-28 17:07:26 -0500

    actualiza app.module ssl

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,8 @@ import { EquipoModule } from './equipo/equipo.module';
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        ssl: process.env.NODE_ENV === 'production' ? {rejectUnauthorized: false} : false,
+        ssl: true,
+        // ssl: process.env.NODE_ENV === 'production' ? {rejectUnauthorized: false} : false,
         autoLoadEntities: true,
         synchronize: true,
         // entities: [User]
