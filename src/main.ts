1ddd71ddf9a45965afa0d0e5d678b8b556e984f3
Ivan Arturo <ivandiaz94ing@gmail.com>
2026-07-31 16:38:50 -0500

    Habilita CORS para que reciba peticiones del frontend subido a GCP

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
   }));
   app.enableCors({
     origin: ['http://localhost:4200'],
+    origin: ['https://frontend-helpdesk-54750791481.southamerica-east1.run.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   });
