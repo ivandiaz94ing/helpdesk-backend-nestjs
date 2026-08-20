44e4a6911b9af3de21052bbecee188ef36cbbed8
Ivan Arturo <ivandiaz94ing@gmail.com>
2026-08-20 15:43:51 -0500

    feat(Ticket) agrega propiedad status al dto update

--- src/ticket/dto/update-ticket.dto.ts
+++ src/ticket/dto/update-ticket.dto.ts
@@ -1,9 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTicketDto } from './create-ticket.dto';
-import { IsOptional, IsUUID } from 'class-validator';
+import { IsEnum, IsOptional, IsUUID } from 'class-validator';
+import { TicketStatus } from '../enums';
 
 export class UpdateTicketDto extends PartialType(CreateTicketDto) {
     @IsOptional()
     @IsUUID()
     tecnicoId?: string;
+
+    @IsOptional()
+    @IsEnum(TicketStatus, { message: `status must be one of the following values: ${Object. values(TicketStatus).join(', ')}` }) 
+    status?:TicketStatus;
 }
